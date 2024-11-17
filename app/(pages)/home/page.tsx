03beec94a00612e718e085dbb834b139237b0eaf
neels22 <indraneelsarode@gmail.com>
2024-11-17 16:52:20 +0530

    footer and navbar

--- app/(pages)/home/page.tsx
+++ app/(pages)/home/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     return(
         <div>
             hi from home
-            <Contact/>
+
 
         </div>
     )
---
+++ app/(pages)/page.tsx
@@ -0,0 +1,17 @@
+import Contact from "./contact/page";
+import Home from "./home/page";
+
+
+
+export default function Layout() {
+    
+    return(
+
+        <div>
+            <Home></Home>
+            <Contact></Contact>
+        </div>
+
+    )
+
+}
